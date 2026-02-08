964d81e76ab69161f17a2b5f3d549f8777196097
Fricdix <elcazatitanes.14@gmail.com>
2026-02-08 17:13:00 -0500

    Arreglo de error de archivo

---
+++ lib/app-config.ts
@@ -0,0 +1,5 @@
+export const APP_CONFIG = {
+  title: "Panel de Inteligencia de Negocios",
+  subtitle: "Analisis del Mercado Tecnologico del Ecuador - MVP",
+  scope: "Analisis agregado a nivel pais (Ecuador)",
+};
